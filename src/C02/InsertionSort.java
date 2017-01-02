c79e8a59a9b6436eb740e458dcde3780f0ed5363
coldraincn <handan213@gmail.com>
2017-01-02 16:33:29 +0800

    insertsort

--- src/C02/InsertionSort.java
+++ src/C02/InsertionSort.java
@@ -14,9 +14,10 @@ public class InsertionSort {
     }
     private static void show(Comparable[] a){
         for (int i = 0;i<a.length;i++){
-            StdOut.print(a[i]+" ");
+            System.out.println(a[i]+" ");
+
         }
-        StdOut.print();
+
     }
     public static boolean isSorted(Comparable[] a){
         for(int i=1;i<a.length;i++){
@@ -27,10 +28,41 @@ public class InsertionSort {
         }
         return true;
     }
+
+    public static void sort(Comparable[] a){
+        for (int i=1;i<a.length;i++){
+
+            for(int j=i;j>0;j--){
+                if(less(a[j],a[j-1])){
+                    exch(a,j,j-1);
+
+                }
+            }
+        }
+
+    }
+    //clrs
+    public static void sort2(Comparable[] a){
+        for (int j=1;j<a.length;j++){
+            Comparable key=a[j];
+            int i=j-1;
+            while (i>=0&&(less(key,a[i]))){
+                a[i+1]=a[i];
+                i--;
+            }
+            a[i+1]=key;
+
+        }
+
+    }
     public static void main(String[] args) {
-        String[] a=In.readString();
-        Sort(a);
-        assert isSorted(a);
+       // String[] a=In.readString();
+       // Sort(a);
+       // assert isSorted(a);
+       // show(a);
+        Integer[] a={6,3,2,5,4,7,9,7};
+       // Integer[] a={6,3,2};
+        sort2(a);
         show(a);
     }
 
